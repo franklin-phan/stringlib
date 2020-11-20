86aa276f4dd79364e5edd3ad7e4a16c4e3c4cc97
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-11-19 18:16:02 -0600

    started jest tests

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+  },
+  env: {
+    "jest/globals": true
+  }
+};
--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-const str = "hello world hello world"
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+const str = 'hello world hello world';
 
 /// makes the first character of a given string uppercase
-String.prototype.capitalize = function(){
-    var first = this.charAt(0)
-    var firstCap = this.replace(first,this.charAt(0).toUpperCase())
-    return firstCap
-}
+// eslint-disable-next-line no-extend-native
+String.prototype.capitalize = function () {
+  const first = this.charAt(0);
+  const firstCap = this.replace(first, this.charAt(0).toUpperCase());
+  return firstCap;
+};
 
-console.log(str.capitalize())
+console.log(str.capitalize());
 /// makes all characters uppercase
 
-String.prototype.allCaps = function() {
-    return this.toUpperCase()
-}
-console.log(str.allCaps())
+// eslint-disable-next-line no-extend-native
+String.prototype.allCaps = function () {
+  return this.toUpperCase();
+};
+console.log(str.allCaps());
 
 /// makes the first character of each word uppercase
 
-String.prototype.capitalizeWords = function() {
-    let string = this.toLowerCase()
-    return string.split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')
-}
+// eslint-disable-next-line no-extend-native
+String.prototype.capitalizeWords = function () {
+  const string = this.toLowerCase();
+  return string.split(' ').map((s) => s.capitalize()).join(' ');
+};
 
-console.log(str.capitalizeWords())
+console.log(str.capitalizeWords());
 
-/// Removes all spaces from the beginning and end of a String 
+/// Removes all spaces from the beginning and end of a String
 /// along with any extra spaces in the middle
+const res = '   hello  world     hello  world     ';
+// eslint-disable-next-line no-extend-native
+String.prototype.removeExtraSpaces = function () {
+  const string = this.trim();
+  return string.split(' ').filter((s) => s !== '').join(' ');
+};
+console.log(res.removeExtraSpaces());
 
-/// Removes extra spaces and replaces spaces with the hyphen "-", 
+/// Removes extra spaces and replaces spaces with the hyphen "-",
 /// and makes all characters lowercase
 
-
-/// Removes extra space and replaces spaces with an underscore "_", 
+/// Removes extra space and replaces spaces with an underscore "_",
 /// and makes all characters lowercase
 
-/// Lowercases the first character of the first word. 
+/// Lowercases the first character of the first word.
 /// Then uppercases the first character of all other words, and removes all spaces
 
-///this method will take the first character of a string and move to the end of a string
+/// this method will take the first character of a string and move to the end of a string
