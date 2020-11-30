ab2ba699da2723497da01c847cab95aed16dc5fa
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-11-30 17:15:41 -0600

    finished challenges with test

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-console */
-/* eslint-disable func-names */
 const str = 'hello world hello world';
 
-/// makes the first character of a given string uppercase
-// eslint-disable-next-line no-extend-native
+//Challenge 1
 String.prototype.capitalize = function () {
   const first = this.charAt(0);
   const firstCap = this.replace(first, this.charAt(0).toUpperCase());
@@ -12,16 +9,14 @@ String.prototype.capitalize = function () {
 
 console.log(str.capitalize());
 /// makes all characters uppercase
-
-// eslint-disable-next-line no-extend-native
+//Challenge 2
 String.prototype.allCaps = function () {
   return this.toUpperCase();
 };
 console.log(str.allCaps());
 
 /// makes the first character of each word uppercase
-
-// eslint-disable-next-line no-extend-native
+//Challenge 3
 String.prototype.capitalizeWords = function () {
   const string = this.toLowerCase();
   return string.split(' ').map((s) => s.capitalize()).join(' ');
@@ -32,7 +27,7 @@ console.log(str.capitalizeWords());
 /// Removes all spaces from the beginning and end of a String
 /// along with any extra spaces in the middle
 const res = '   hello  world     hello  world     ';
-// eslint-disable-next-line no-extend-native
+//Challenge 4
 String.prototype.removeExtraSpaces = function () {
   const string = this.trim();
   return string.split(' ').filter((s) => s !== '').join(' ');
@@ -41,11 +36,44 @@ console.log(res.removeExtraSpaces());
 
 /// Removes extra spaces and replaces spaces with the hyphen "-",
 /// and makes all characters lowercase
+//Challenge 5
+const UP = 'HELLO HELLO!@:#$ Hello'
+String.prototype.kabobCase = function () {
+  const lowercase = this.toLowerCase().replace(/[^a-zA-Z0-9]/g, ' ');
+  return lowercase.split(' ').filter((s) => s !== '').join('-')
+}
+console.log(UP.kabobCase());
 
 /// Removes extra space and replaces spaces with an underscore "_",
 /// and makes all characters lowercase
+//Challenge 6
+
+String.prototype.snakeCase = function () {
+  const lowercase = this.toLowerCase().replace(/[^a-zA-Z0-9]/g, ' ');
+  return lowercase.split(' ').filter((s) => s !== '').join('_');
+}
+console.log(UP.snakeCase());
 
 /// Lowercases the first character of the first word.
 /// Then uppercases the first character of all other words, and removes all spaces
+//Challenge 7
+const CAMEL = 'Hello world hello world'
+
+String.prototype.camelCase = function () {
+  const string = this.toLowerCase().replace(/[^a-zA-Z0-9]/g, ' ').split(' ').map((s) => s.capitalize()).join(' ');
+  const first = string.charAt(0);
+  const firstLow = string.replace(first, string.charAt(0).toLowerCase()).split(' ').filter((s) => s !== '').join('');
+  return firstLow
+}
+
+console.log(CAMEL.camelCase());
 
 /// this method will take the first character of a string and move to the end of a string
+//Challenge 8
+
+String.prototype.shift = function () {
+  const first = this.charAt(0);
+  const firstCap = this.replace(first,'');
+  const shift = firstCap.concat(first)
+  return shift;
+}
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,64 @@
-test('Sanity check', () => {
 
-});
+test('Sanity Check', () => {
+  expect(2+2).toBe(4)
+})
+
+const sl = require('../index')
+
+  test('Test capitalize', () => {
+    expect('hello'.capitalize()).toBe('Hello')
+    expect('hi'.capitalize()).toBe('Hi')
+    expect('cat'.capitalize()).toBe('Cat')
+    expect('dog'.capitalize()).toBe('Dog')
+  })
+
+  test('Test allCaps', () => {
+    expect('hello'.allCaps()).toBe('HELLO')
+    expect('hi'.allCaps()).toBe('HI')
+    expect('cat'.allCaps()).toBe('CAT')
+    expect('dog'.allCaps()).toBe('DOG')
+  })
+
+  test('Test capitalizeWords', () => {
+    expect('hello hello hello'.capitalizeWords()).toBe('Hello Hello Hello')
+    expect('hi hi hi'.capitalizeWords()).toBe('Hi Hi Hi')
+    expect('cat Cat Cat'.capitalizeWords()).toBe('Cat Cat Cat')
+    expect('dog Dog Dog'.capitalizeWords()).toBe('Dog Dog Dog')
+  })
+
+
+  test('Test removeExtraSpaces', () => {
+    expect('   hello  world     hello  world     '.removeExtraSpaces()).toBe('hello world hello world')
+    expect('  Cat Cat Cat  Cat'.removeExtraSpaces()).toBe('Cat Cat Cat Cat')
+    expect('        Dog Dog      Dog     Dog'.removeExtraSpaces()).toBe('Dog Dog Dog Dog')
+    expect('Green Red     Blue'.removeExtraSpaces()).toBe('Green Red Blue')
+  })
+
+  test('Test kabobCase', () => {
+    expect('HELLO HELLO!@:#$ Hello'.kabobCase()).toBe('hello-hello-hello')
+    expect('cat Cat cat'.kabobCase()).toBe('cat-cat-cat')
+    expect('dog dog! Dog'.kabobCase()).toBe('dog-dog-dog')
+    expect('RED! BLUE! GREEN!'.kabobCase()).toBe('red-blue-green')
+  })
+
+  test('Test snakeCase', () => {
+    expect('HELLO HELLO!@:#$ Hello'.snakeCase()).toBe('hello_hello_hello')
+    expect('cat Cat cat'.snakeCase()).toBe('cat_cat_cat')
+    expect('dog dog! Dog'.snakeCase()).toBe('dog_dog_dog')
+    expect('RED! BLUE! GREEN!'.snakeCase()).toBe('red_blue_green')
+  })
+
+  test('Test camelCase', () => {
+    expect('HELLO HELLO!@:#$ Hello'.camelCase()).toBe('helloHelloHello')
+    expect('cat Cat cat'.camelCase()).toBe('catCatCat')
+    expect('dog dog! Dog'.camelCase()).toBe('dogDogDog')
+    expect('RED! BLUE! GREEN!'.camelCase()).toBe('redBlueGreen')
+  })
+
+  test('Test shift', () => {
+    expect('Hello World'.shift()).toBe('ello WorldH')
+    expect('Cat World'.shift()).toBe('at WorldC')
+    expect('dog World'.shift()).toBe('og Worldd')
+    expect('Green World'.shift()).toBe('reen WorldG')
+
+  })
